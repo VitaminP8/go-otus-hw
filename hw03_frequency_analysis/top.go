610a466b5434b9df32845dc47d3d737247508a97
Alexey <alexey.zhukovich@gmail.com>
2025-02-18 12:23:11 +0300

    hw 2 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,35 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"slices"
+	"strings"
+)
+
+func Top10(s string) []string {
+	words := strings.Fields(s)
+	wordsFreq := make(map[string]int)
+	uniqWords := make([]string, 0, len(wordsFreq))
+
+	for _, word := range words {
+		wordsFreq[word]++
+	}
+
+	for word := range wordsFreq {
+		uniqWords = append(uniqWords, word)
+	}
+
+	slices.SortFunc(uniqWords, func(a, b string) int {
+		if wordsFreq[a] != wordsFreq[b] {
+			return wordsFreq[b] - wordsFreq[a]
+		}
+		if a < b {
+			return -1
+		}
+		return 1
+	})
+
+	if len(uniqWords) > 10 {
+		uniqWords = uniqWords[:10]
+	}
+	return uniqWords
 }
