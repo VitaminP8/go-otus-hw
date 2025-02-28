1c65314bffc3ca1ba9a40003674323bec48a0af2
Alexey <alexey.zhukovich@gmail.com>
2025-02-28 21:03:38 +0300

    complete hw04

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,54 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	keys     map[*ListItem]Key
+	// Я не хочу хранить в ListItem.Value ключ и значение, поэтому сделал этот словарь чтобы можно было получить
+	// ключ по *ListItem
+}
+
+func (c *lruCache) Set(key Key, value any) bool {
+	// если элемент уже есть - перемещаем в начало
+	listItem, exists := c.items[key]
+	if exists {
+		listItem.Value = value
+		c.queue.MoveToFront(listItem)
+
+		return true
+	}
+
+	// добавляем элемент в начало
+	listItem = c.queue.PushFront(value)
+	c.items[key] = listItem
+	c.keys[listItem] = key
+
+	// если размер очереди > вместимости кэша - удаляем последний элемент
+	if c.capacity < c.queue.Len() {
+		lastListItem := c.queue.Back()
+		keyToRemove := c.keys[lastListItem]
+		delete(c.items, keyToRemove)
+		delete(c.keys, lastListItem)
+		c.queue.Remove(lastListItem)
+	}
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	listItem, exists := c.items[key]
+	if exists {
+		c.queue.MoveToFront(listItem)
+		return listItem.Value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.keys = make(map[*ListItem]Key, c.capacity)
+	c.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
@@ -21,5 +64,6 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		keys:     make(map[*ListItem]Key, capacity),
 	}
 }
