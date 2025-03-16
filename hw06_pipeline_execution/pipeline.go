c36c9f71cab39c6134f7c4623f7c3af30460b9fc
Alexey <alexey.zhukovich@gmail.com>
2025-03-16 22:46:09 +0300

    HW2 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,65 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	// начиная с входного канала, проходимся по всем стейджам и связываем их
+	out := in
+	for _, stage := range stages {
+		out = doStage(out, done, stage)
+	}
+
+	return out
+}
+
+func doStage(in In, done In, stage Stage) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		for value := range in {
+			select {
+			case <-done:
+				<-in
+				return
+			default:
+				// обрабатываем данные с помощью stage (передаем канал, содержащий только value и учитывающий done)
+				stageOut := stage(wrapValueToChan(value, done))
+
+				select {
+				case <-done:
+					return
+				case newValue, ok := <-stageOut:
+					if !ok {
+						return
+					}
+					select {
+					case <-done:
+						return
+					case out <- newValue:
+					}
+				}
+			}
+		}
+	}()
+	return out
+}
+
+func wrapValueToChan(value any, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		select {
+		case <-done:
+			return
+		case out <- value:
+		}
+	}()
+
+	return out
 }
